102d40b0bd63c9131e3e55ddaf0c758ed57f25ed
WJ5 <elan_zh@163.com>
2018-05-27 13:27:06 +0800

    初始化

---
+++ src/main/java/cn/zh/elan/App.java
@@ -0,0 +1,19 @@
+package cn.zh.elan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+    @RequestMapping(value = "/hello", produces = "text/plain;charset=UTF-8")
+    String hello() {
+        return "Hello Spring Boot!";
+    }
+}
